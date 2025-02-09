438af66a1b1c912f402446587f3f4047cf33d1d8
jlhardyphd <jlhardyphd@gmail.com>
2025-02-09 12:28:06 -0800

    Added a few more packages

--- work_with_dicom.py
+++ work_with_dicom.py
@@ -1,1 +1,5 @@
 import tensorflow as tf
+import pydicom  # Main library for DICOM handling
+import nibabel  # For neuroimaging formats
+from monai.data import Dataset  # Medical imaging AI framework
+
